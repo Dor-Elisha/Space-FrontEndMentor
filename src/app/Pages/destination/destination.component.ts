b4e419841e3e08f1dd9af2ef49fc935b01b68ac8
Dor Elisha <Dorelishaelisha@gmail.com>
2021-11-26 03:27:27 +0200

    Finished Destination page
    Added - responsive css for "Destination" page.
    fixed "home" page with responsive css

--- src/app/Pages/destination/destination.component.ts
+++ src/app/Pages/destination/destination.component.ts
@@ -10,14 +10,42 @@ export class DestinationComponent implements OnInit {
 
   constructor( private http: HttpClient) { }
 
-  projectData!: any;
+  allPlanetData!: any;
+  planetData!: any;
+
+  imageDirection!: string;
+  starName!: string;
+  description!: string;
+  distance!: string;
+  travelTime!: string;
+
+
+
   ngOnInit(): void {
+
     this.http.get("assets/data.json").subscribe( data => {
       JSON.stringify(data)
-      this.projectData = data;
-      this.projectData = this.projectData.crew;
-      console.log(this.projectData);
+      this.allPlanetData = data;
+      console.log(this.allPlanetData);
+      this.allPlanetData = this.allPlanetData.destinations;
+      console.log(this.allPlanetData[0]);
+
+      this.imageDirection = this.allPlanetData[0].images.png;
+      this.imageDirection = this.imageDirection.replace(/^.\//i, "");
+      this.starName = this.allPlanetData[0].name;
+      this.description = this.allPlanetData[0].description;
+      this.distance = this.allPlanetData[0].distance;
+      this.travelTime = this.allPlanetData[0].travel;
     })
   }
 
+  getPlanetData(planetNum: number): void {
+      this.planetData = this.allPlanetData[planetNum];
+      this.imageDirection = this.planetData.images.webp;
+      this.starName = this.planetData.name;
+      this.description = this.planetData.description;
+      this.distance = this.planetData.distance;
+      this.travelTime = this.planetData.travel;
+
+    }
 }
